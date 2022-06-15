c213737192ead6cdbb4c8595a87a40889ffb0b51
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-06-14 22:59:27 -0600

    feat(app): listen and serve hello world

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,11 +1,49 @@
 package app
 
 import (
-	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"github.com/berkeleytrue/crypto-agg-go/config"
+	"github.com/gin-gonic/gin"
 )
 
+var wg sync.WaitGroup
+
+type Server struct {
+	server *http.Server
+}
+
+func (s *Server) start() {
+	wg.Add(1)
+	go func() {
+		s.server.ListenAndServe()
+	}()
+}
+
 func Run(cfg *config.Config) {
-	fmt.Printf("Hello World %s", cfg)
+	handler := gin.Default()
+	handler.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "hello world"})
+	})
+
+	httpServer := &http.Server{
+		Handler: handler,
+	}
+
+	httpServer.Addr = net.JoinHostPort("", cfg.HTTP.Port)
+
+	s := &Server{
+		server: httpServer,
+	}
+
+	s.start()
+	wg.Wait()
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
 }
