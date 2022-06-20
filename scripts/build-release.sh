6c23015f9e5467d7de40c5c8e85a6fefe3ee9446
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-06-19 21:29:13 -0600

    fix(build): better build output

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -1,9 +1,13 @@
 IPATH="github.com/berkeleytrue/crypto-egg-go/config"
+HASH=$(git rev-parse --short HEAD)
+USER=$(id -u -n)
 
 mkdir -pv build
 
 go build -ldflags="-X $IPATH.Port=10000 \
   -X $IPATH.GinReleaseMode=true \
-  -X '$IPATH.Hash=$(git rev-parse --short HEAD)'
+  -X '$IPATH.Hash=$HASH'
   -X '$IPATH.Time=$(date)' \
-  -X '$IPATH.User=$(id -u -n)'" -o ./build ./cmd/app ./cmd/cli
+  -X '$IPATH.User=$USER'" -o ./build ./cmd/app ./cmd/cli
+
+echo "Build Complete: Hash: $HASH, User: $USER"
