22845147f2614597b3d3892d49e3cf916bb183a8
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-06-19 21:26:18 -0600

    feat(cli): add gas

--- cmd/gas.go
+++ cmd/gas.go
@@ -7,7 +7,10 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/berkeleytrue/crypto-egg-go/internal/core/domain"
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+	"gopkg.in/h2non/gentleman.v2"
 )
 
 // gasCmd represents the gas command
@@ -16,7 +19,37 @@ var gasCmd = &cobra.Command{
 	Short: "Get current gas base fee for Ethereum",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("gas called")
+		logger := zap.NewExample().Sugar()
+		defer logger.Sync()
+
+		client := cmd.Context().Value("client").(*gentleman.Client)
+		request := client.Request()
+
+		request.AddPath("/api/gas")
+
+		res, err := request.Send()
+
+		if err != nil {
+			logger.Fatal("Request failed: %w", err)
+		}
+
+		if !res.Ok && res.StatusCode != 404 {
+			logger.Fatalf("Bad response: %d", res.StatusCode)
+		}
+
+		if res.StatusCode == 404 {
+			fmt.Println("0")
+			return
+		}
+
+		gas := domain.Gas{}
+		err = res.JSON(&gas)
+		if err != nil {
+			logger.Fatal("Couldn't parse response: %w", err)
+		}
+
+		fmt.Printf("%.2f\n", gas.Base)
+
 	},
 }
 
